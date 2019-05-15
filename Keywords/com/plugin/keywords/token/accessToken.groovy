caad815a944a7a141dc4713fcf2b85081ea501cf
jayeshpansareindex9 <50333829+jayeshpansareindex9@users.noreply.github.com>
2019-05-15 15:26:42 +0530

    Added KS Plugin
    Get Authorization Value From API Keywords plugin is added.

---
+++ Keywords/com/plugin/keywords/token/accessToken.groovy
@@ -0,0 +1,67 @@
+package com.plugin.keywords.token
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testdata.TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords
+
+import internal.GlobalVariable
+
+import MobileBuiltInKeywords as Mobile
+import WSBuiltInKeywords as WS
+import WebUiBuiltInKeywords as WebUI
+
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.By
+
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
+import com.kms.katalon.core.webui.driver.DriverFactory
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObjectProperty
+
+import com.kms.katalon.core.mobile.helper.MobileElementCommonHelper
+import com.kms.katalon.core.util.KeywordUtil
+
+import com.kms.katalon.core.webui.exception.WebElementNotFoundException
+
+import groovy.json.JsonSlurper
+
+class accessToken {
+	/**
+	 * Created By: jayesh
+	 * Date: 11-10-2018
+	 * Description:  get session from login API
+	 * **/
+	@Keyword
+	def getAccessTokenFromAPI(def accessTokenKey, ResponseObject APIResult) {
+		def slurper = new JsonSlurper()
+		if(APIResult){
+			Map  APIResponce = slurper.parseText(APIResult.getResponseBodyContent())
+			boolean isKeyPresent = APIResponce.get(accessTokenKey)
+			if (isKeyPresent) {
+				return APIResponce.get(accessTokenKey)
+			}else{
+				return "Key is not present"
+			}
+		}else{
+			return "Error: Parsing error.";
+		}
+	}
+}
